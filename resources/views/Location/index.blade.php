27100994d07b6af9ee983e37bbe371ce0ed61571
joyanta1000 <joyanta955@gmail.com>
2022-03-08 18:43:27 +0600

    maps work

--- resources/views/Location/index.blade.php
+++ resources/views/Location/index.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>laravel 8 Get Data From Database using Ajax - Tutsmake.com</title>
+  <title>Location</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
@@ -36,35 +36,10 @@
     </select>
   </div>
 
-
   <br>
   <br>
 
   <div id="googleMap" style="width: 100%; height: 640px"></div>
-
-  <!-- <br>
-
-  <div>
-    My Location To:
-    <select name="" id="location">
-      <option value="">Select</option>
-      @foreach ($locations as $location)
-      <option value="{{$location->id}}">{{$location->location}}</option>
-      @endforeach
-    </select>
-  </div>
-
-  <br> -->
-
-  <!-- <p>Click the button to get your coordinates.</p>
-
-  <button onclick="getLocation()">Try It</button>
-
-  <p id="demo"></p> -->
-
-
-
-
 
 </body>
 
@@ -81,25 +56,18 @@
 
   let selected = 10;
 
+  let markersArray = [];
+
+  let myMarkersArray = [];
+
   $('#selectDis').change(function() {
     this.selected = $(this).val();
-    // console.log($(this).val());
 
     selected = this.selected;
 
-
-    initMap();
-
+    getLocation();
 
   });
-
-
-  function r() {
-    console.log(selected);
-  }
-
-
-
 
   function haversine_distance(mk1, mk2) {
     var R = 6371; // Radius of the Earth in km , if mile then 3959
@@ -127,120 +95,29 @@
           lng: parseFloat(data.OwnLocation.lng)
         },
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        // zoom: 13,
 
       });
 
-      // details
+      myLocation();
+
+    });
+
+  }
+
+  function myLocation() {
 
 
-      //     const request = {
-      //   placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
-      //   fields: ["name", "formatted_address", "place_id", "geometry"],
-      // };
-      // const infowindow = new google.maps.InfoWindow();
-      // const service = new google.maps.places.PlacesService(map);
+    deleteOverlays();
 
-      // service.getDetails(request, (place, status) => {
-      //   if (
-      //     status === google.maps.places.PlacesServiceStatus.OK &&
-      //     place &&
-      //     place.geometry &&
-      //     place.geometry.location
-      //   ) {
-      //     const marker = new google.maps.Marker({
-      //       map,
-      //       position: place.geometry.location,
-      //     });
+    $.get('Location/show', function(data) {
 
-      //     google.maps.event.addListener(marker, "click", () => {
-      //       const content = document.createElement("div");
-      //       const nameElement = document.createElement("h2");
-
-      //       nameElement.textContent = place.name;
-      //       content.appendChild(nameElement);
-
-      //       const placeIdElement = document.createElement("p");
-
-      //       placeIdElement.textContent = place.place_id;
-      //       content.appendChild(placeIdElement);
-
-      //       const placeAddressElement = document.createElement("p");
-
-      //       placeAddressElement.textContent = place.formatted_address;
-      //       content.appendChild(placeAddressElement);
-      //       infowindow.setContent(content);
-      //       infowindow.open(map, marker);
-      //     });
-      //   }
-      // });
-
-      // details
-
-      var array = [];
-
-      // function dataURLtoFile(dataurl, icon) {
-
-      //   var arr = dataurl.split(','),
-      //     mime = arr[0].match(/:(.*?);/)[1],
-      //     bstr = atob(arr[1]),
-      //     n = bstr.length,
-      //     u8arr = new Uint8Array(n);
-
-      //   while (n--) {
-      //     u8arr[n] = bstr.charCodeAt(n);
-      //   }
-
-      //   return new File([u8arr], icon, {type:mime});
-      // }
-
-
-      // imageMy = function(icon) {
-
-
-      //   let imageFile = icon;
-      //   var reader = new FileReader();
-
-      //   // reader.onload = function (e) {
-      //   var img = url(icon);
-
-      //   // console.log(img);
-      //   // img.onload = function (event) {
-      //   // Dynamically create a canvas element
-      //   var canvas = document.createElement("canvas");
-
-      //   // var canvas = document.getElementById("canvas");
-      //   var ctx = canvas.getContext("2d");
-
-      //   // Actual resizing
-      //   ctx.drawImage(img, 1000, 1000);
-
-      //   // Show resized image in preview element
-      //   var dataurl = canvas.toDataURL(imageFile.type);
-      //   // document.getElementById("preview").src = dataurl;
-
-      //   console.log(img, 'img');
-
-      //   console.log(dataURLtoFile(img, icon));
-
-      //   // base 64 decoding
-
-
-
-      //   return dataURLtoFile(dataurl, icon);
-
-      //   // console.log();
-      //   // }
-
-      //   // }
-      //   // return dataurl;
-      //   // img.src = e.target.result;
-
-      // }
+      console.log(data, 'dataMyMy');
 
       var infoWindow = new google.maps.InfoWindow();
 
       var latlngbounds = new google.maps.LatLngBounds();
+
+      var destinationDistance = [];
 
       for (let i = 0; i < data.OtherLocation.length; i++) {
 
@@ -253,30 +130,19 @@
             lng: parseFloat(data.OwnLocation.lng)
           },
           map: map,
-          title: 'Your Location'
+          title: 'Your Location',
+          icon: 'https://raw.githubusercontent.com/Joyanta1000/map/master/icons/human-location.png'
         });
-        (function (mk1, data2) {
-                google.maps.event.addListener(mk1, "click", function (e) {
-                    infoWindow.setContent("<div style = 'width:200px;min-height:40px'>My Location</div>");
-                    infoWindow.open(map, mk1);
-                });
-            })(mk1, data2);
- 
-            //Extend each marker's position in LatLngBounds object.
-            latlngbounds.extend(mk1.position);
-        // window.ctx = canvas.getContext("2d");
+        (function(mk1, data2) {
+          google.maps.event.addListener(mk1, "click", function(e) {
+            infoWindow.setContent("<div style = 'width:200px;min-height:40px'>My Location</div>");
+            infoWindow.open(map, mk1);
+          });
+        })(mk1, data2);
 
-        // var mk2 = new google.maps.Marker({
-        //   position: {
-        //     lat: parseFloat(data.OtherLocation[i].lat),
-        //     lng: parseFloat(data.OtherLocation[i].lng)
-        //   },
-        //   map: map,
-        //   title: data.OtherLocation[i].location,
-        //   icon: data.OtherLocation[i].icon
-        //   // imageMy(data.OtherLocation[i].icon).name
-        //   // data.OtherLocation[i].icon
-        // });
+        latlngbounds.extend(mk1.position);
+
+        myMarkersArray.push(mk1);
 
         var mk2 = new google.maps.Marker({
           position: {
@@ -287,62 +153,62 @@
           title: data3.location,
           icon: 'https://img.icons8.com/ios-filled/50/000000/marker-h.png'
         });
-        (function (mk2, data3) {
-                google.maps.event.addListener(mk2, "click", function (e) {
-                    infoWindow.setContent("<div style = 'width:auto;min-height:auto; font-family:sans-sarif; font-size: 30px;'><img class='center' src='"+ data3.icon +"'  width='auto' height='auto'><br><br> " + data3.location + "</div>");
-                    infoWindow.open(map, mk2);
-                });
-            })(mk2, data3);
- 
-            //Extend each marker's position in LatLngBounds object.
-            latlngbounds.extend(mk2.position);
+
         var distance = haversine_distance(mk1, mk2);
 
+        destinationDistance.push(distance);
 
-        console.log(distance, parseFloat(selected));
+        console.log(destinationDistance, parseFloat(selected));
 
+        (function(mk2, data3) {
+          var icon = {
+            url: data3.icon,
+            scaledSize: new google.maps.Size(10, 10),
+            radius: 50,
 
+          };
+          google.maps.event.addListener(mk2, "click", function(e) {
+            infoWindow.setContent("<div style='text-align:center;'><div style = 'width:auto; height:auto; font-family:sans-sarif; font-size: 20px;'><img class='center' src='" + icon.url + "' style='width:40%; height:auto; display: block; margin-left: auto; margin-right: auto;'><br><br> " + data3.location + "<br><br><span style='color: blue;'> " + destinationDistance[i] + " Km From My Location </span></div></div>");
+            infoWindow.open(map, mk2);
+          });
+        })(mk2, data3);
 
-        if (distance > selected) {
+        latlngbounds.extend(mk2.position);
+
+        markersArray.push(mk2);
+
+        if (destinationDistance[i] > selected) {
+          mk2.setVisible(false)
           mk2.setMap(null);
           break;
         }
-
-        array.push(mk2);
-
-        var markers = mk2; //some array
-
-      // console.log(markers.length, 'ma');
-      // var bounds = new google.maps.LatLngBounds();
-      // for (var j = 0; j < 10; j++) {
-      //   bounds.extend(markers[j]);
-      // }
-
-      // map.fitBounds(bounds);
-
-        
-
-      }
-
-      var bounds = new google.maps.LatLngBounds();
- 
-        //Center map and adjust Zoom based on the position of all markers.
+        var bounds = new google.maps.LatLngBounds();
         map.setCenter(latlngbounds.getCenter());
         map.fitBounds(latlngbounds);
 
-
-      
-
-
+      }
 
     });
 
   }
 
-  // setInterval(function() {
-    getLocation();
-  // }, 1000);
+  function deleteOverlays() {
 
+    if (myMarkersArray) {
+      for (i in myMarkersArray) {
+        myMarkersArray[i].setMap(null);
+      }
+      myMarkersArray.length = 0;
+    }
+    if (markersArray) {
+      for (i in markersArray) {
+        markersArray[i].setMap(null);
+      }
+      markersArray.length = 0;
+    }
+  }
+
+  getLocation();
 
   function getLocation() {
     if (navigator.geolocation) {
@@ -353,8 +219,6 @@
   }
 
   function showPosition(position) {
-    // x.innerHTML = "Latitude: " + position.coords.latitude +
-    //   "<br>Longitude: " + position.coords.longitude;
 
     $.ajax({
       method: "PUT",
@@ -364,7 +228,7 @@
         lng: position.coords.longitude,
       },
       success: function(result) {
-
+        myLocation();
       }
     });
 
